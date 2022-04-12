9392a0ad687a7c17fa311587133cad2bd0cfefdd
Pascal Huber <pascal.huber@resolved.ch>
2022-04-12 11:23:00 +0200

    add newlft() in vir_low

--- vir/defs/middle/ast/statement.rs
+++ vir/defs/middle/ast/statement.rs
@@ -177,9 +177,9 @@ pub struct Assign {
     pub position: Position,
 }
 
-#[display(fmt = "{} = newlft()", name)]
+#[display(fmt = "{} = newlft()", target)]
 pub struct NewLft {
-    pub name: String,
+    pub target: VariableDecl,
     pub position: Position,
 }
 
