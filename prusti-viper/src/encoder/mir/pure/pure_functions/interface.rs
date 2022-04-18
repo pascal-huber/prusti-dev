d0e7038403ea16eff9eaeca1c528c68fca263e61
Pascal Huber <pascal.huber@resolved.ch>
2022-04-18 11:41:35 +0200

    add assert again

--- prusti-viper/src/encoder/mir/pure/pure_functions/interface.rs
+++ prusti-viper/src/encoder/mir/pure/pure_functions/interface.rs
@@ -516,16 +516,16 @@ impl<'v, 'tcx: 'v> PureFunctionEncoderInterface<'v, 'tcx>
 
     fn register_function_constructor_mir(
         &self,
-        _function_identifier: String,
-        _constructor: FunctionConstructor<'v, 'tcx>,
+        function_identifier: String,
+        constructor: FunctionConstructor<'v, 'tcx>,
     ) -> SpannedEncodingResult<()> {
         // TODO: this causes a problem when we don't have the lifetime name in the reference (LifetimeConst vs Uniqueness)
-        // assert!(self
-        //     .pure_function_encoder_state
-        //     .function_constructors
-        //     .borrow_mut()
-        //     .insert(function_identifier, constructor)
-        //     .is_none());
+        assert!(self
+            .pure_function_encoder_state
+            .function_constructors
+            .borrow_mut()
+            .insert(function_identifier, constructor)
+            .is_none());
         Ok(())
     }
 }
