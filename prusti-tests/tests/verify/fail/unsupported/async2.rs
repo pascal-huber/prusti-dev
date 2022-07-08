8f76f426880a4c77181e83a634f874373788fa40
Pascal Huber <pascal.huber@resolved.ch>
2022-07-08 21:30:27 +0200

    fix async tests cont'd

--- prusti-tests/tests/verify/fail/unsupported/async2.rs
+++ prusti-tests/tests/verify/fail/unsupported/async2.rs
@@ -2,7 +2,7 @@ pub struct QueryClient {
 }
 impl QueryClient
 {
-    pub async fn accounts( //~ ERROR unsupported type
+    pub async fn accounts(
         &mut self,
         _request: u32
     ) -> Result<u32, u32> { //~ ERROR generator fields are not supported yet
